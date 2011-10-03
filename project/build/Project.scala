36cad45a61e5d5f0c1fc15ecebac17a47f79d434
FNG <daniel.wydler@gmx.ch>
2011-10-03 20:11:05 +0200

    add jfreechart

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -7,4 +7,5 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.8" % "test" withSources
   val scalaz = "org.scalaz" %% "scalaz-core" % "6.0" withSources
   val scalaSwing = "org.scala-lang" % "scala-swing" % "2.8.1" withSources
+  val jfreechart = "jfree" % "jfreechart" % "1.0.13"
 }
--- src/main/scala/com/github/fng/structurer/SwingTest.scala
+++ src/main/scala/com/github/fng/structurer/SwingTest.scala
@@ -1,6 +1,10 @@
 package com.github.fng.structurer
 
 import swing._
+import org.jfree.chart.{ChartFactory, ChartPanel}
+import org.jfree.data.general.DefaultPieDataset
+import org.jfree.chart.plot.PiePlot3D
+import org.jfree.util.Rotation
 
 object SwingTest extends SimpleSwingApplication {
   def top = new MainFrame {
@@ -14,19 +18,66 @@ object SwingTest extends SimpleSwingApplication {
     minimumSize = new java.awt.Dimension(framewidth, frameheight)
 
 
-    menuBar = new MenuBar{
-      contents += new Menu("File"){
-        contents += new MenuItem(Action("Quit"){
+    menuBar = new MenuBar {
+      contents += new Menu("File") {
+        contents += new MenuItem(Action("Quit") {
           System.exit(0)
         })
       }
-      contents += new Menu("Test"){
-        contents += new MenuItem(Action("Dialog ..."){
+      contents += new Menu("Test") {
+        contents += new MenuItem(Action("Dialog ...") {
           Dialog.showMessage(message = "This is a dialog!")
         })
       }
-
     }
 
+    val firstPanel = new FlowPanel {
+      contents += new Label {
+        text = "firstPanel - A"
+      }
+      contents += new Label {
+        text = "firstPanel - B"
+      }
+    }
+
+    val secondPanel = new FlowPanel {
+      contents += new Label {
+        text = "secondPanel - A"
+      }
+      contents += new Label {
+        text = "secondPanel - B"
+      }
+    }
+
+
+    val chartPanel = new Panel {
+      override lazy val peer = createChartPanel()
+    }
+
+
+    contents = new SplitPane(Orientation.Vertical, firstPanel, chartPanel) {
+      dividerLocation = 250
+      dividerSize = 8
+      oneTouchExpandable = true
+    }
+
+
   }
+
+  def createChartPanel(): ChartPanel = {
+    val dataSet = new DefaultPieDataset()
+    dataSet.setValue("Linux", 29)
+    dataSet.setValue("Mac", 20)
+    dataSet.setValue("Windows", 51)
+
+
+    val chart = ChartFactory.createPieChart3D("Chart title", dataSet, true, true, false)
+    val plot = chart.getPlot.asInstanceOf[PiePlot3D]
+    plot.setStartAngle(290)
+    plot.setDirection(Rotation.CLOCKWISE)
+    plot.setForegroundAlpha(0.5f)
+
+    new ChartPanel(chart)
+  }
+
 }
