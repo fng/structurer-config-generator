382060931a018dae903fe48fba905ff73469e4d0
FNG <daniel.wydler@gmx.ch>
2011-10-04 17:08:06 +0200

    add sample for Captial Protection w/o cap

--- src/main/scala/com/github/fng/structurer/Structurer.scala
+++ src/main/scala/com/github/fng/structurer/Structurer.scala
@@ -38,6 +38,9 @@ object Structurer extends SimpleSwingApplication {
         OptionInstrument(OptionType.Call, 0, 100, OptionBarrierType.NoBarrier),
         OptionInstrument(OptionType.Call, 1.0, 50, OptionBarrierType.NoBarrier),
         OptionInstrument(OptionType.Call, 1.5, -150, OptionBarrierType.NoBarrier)),
+    new SampleMenuItem("Capital Protected w/o Cap",
+      BondInstrument(950, 1),
+      OptionInstrument(OptionType.Call, 1.0, 950, OptionBarrierType.NoBarrier)),
     new SampleMenuItem("Barrier Reverse Convertible",
       BondInstrument(1000, 1),
       OptionInstrument(OptionType.Put, 1.0, -1000, OptionBarrierType.KnockInBarrier))
