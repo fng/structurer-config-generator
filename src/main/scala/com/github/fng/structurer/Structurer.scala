84894209e920b0dd82e822ea9c7c5fa027221488
FNG <daniel.wydler@gmx.ch>
2011-10-04 17:09:52 +0200

    add sample for Captial Protection with cap

--- src/main/scala/com/github/fng/structurer/Structurer.scala
+++ src/main/scala/com/github/fng/structurer/Structurer.scala
@@ -38,12 +38,16 @@ object Structurer extends SimpleSwingApplication {
         OptionInstrument(OptionType.Call, 0, 100, OptionBarrierType.NoBarrier),
         OptionInstrument(OptionType.Call, 1.0, 50, OptionBarrierType.NoBarrier),
         OptionInstrument(OptionType.Call, 1.5, -150, OptionBarrierType.NoBarrier)),
-    new SampleMenuItem("Capital Protected w/o Cap",
-      BondInstrument(950, 1),
-      OptionInstrument(OptionType.Call, 1.0, 950, OptionBarrierType.NoBarrier)),
-    new SampleMenuItem("Barrier Reverse Convertible",
-      BondInstrument(1000, 1),
-      OptionInstrument(OptionType.Put, 1.0, -1000, OptionBarrierType.KnockInBarrier))
+      new SampleMenuItem("Capital Protected w/o Cap",
+        BondInstrument(950, 1),
+        OptionInstrument(OptionType.Call, 1.0, 1000, OptionBarrierType.NoBarrier)),
+      new SampleMenuItem("Capital Protected with Cap",
+        BondInstrument(950, 1),
+        OptionInstrument(OptionType.Call, 1.0, 1000, OptionBarrierType.NoBarrier),
+        OptionInstrument(OptionType.Call, 1.4, -1000, OptionBarrierType.NoBarrier)),
+      new SampleMenuItem("Barrier Reverse Convertible",
+        BondInstrument(1000, 1),
+        OptionInstrument(OptionType.Put, 1.0, -1000, OptionBarrierType.KnockInBarrier))
     )
 
     menuBar = new MenuBar {
