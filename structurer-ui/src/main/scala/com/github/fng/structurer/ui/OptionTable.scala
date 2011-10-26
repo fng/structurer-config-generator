e5abb75d08f1d546b81f041729b6eb1896952929
FNG <daniel.wydler@gmx.ch>
2011-10-26 11:01:51 +0200

    validate ExpressionCells in OptionTable

--- structurer-ui/src/main/scala/com/github/fng/structurer/ui/OptionTable.scala
+++ structurer-ui/src/main/scala/com/github/fng/structurer/ui/OptionTable.scala
@@ -1,14 +1,16 @@
 package com.github.fng.structurer.ui
 
-import instrument.ExpressionOption
+import instrument.TextFieldType.ExpressionField
+import instrument.{TextFieldVerifier, ExpressionOption}
 import swing.Table
 import collection.mutable.Buffer
-import javax.swing.{JComboBox, DefaultCellEditor}
 import javax.swing.table.{TableCellEditor, AbstractTableModel}
 import com.github.fng.structurer.instrument.{OptionBarrierType, OptionType}
 import com.github.fng.structurer.config.expression.{ExpressionParser, RichExpression}
-import table.GenericTableModel
-import table.GenericTableModel.Column
+import com.github.fng.structurer.ui.table.GenericTableModel
+import com.github.fng.structurer.ui.table.GenericTableModel.Column
+import javax.swing._
+import java.awt.Color
 
 class OptionTable(options: List[MutableOption]) extends Table {
 
@@ -16,6 +18,10 @@ class OptionTable(options: List[MutableOption]) extends Table {
   model = tableModel
 
   autoResizeMode = Table.AutoResizeMode.AllColumns
+
+  peer.getColumnModel.getColumn(1).setPreferredWidth(150)
+  peer.getColumnModel.getColumn(2).setPreferredWidth(150)
+  peer.getColumnModel.getColumn(3).setPreferredWidth(100)
 
   def add(option: MutableOption) {
     tableModel.add(option)
@@ -38,10 +44,7 @@ class OptionTable(options: List[MutableOption]) extends Table {
 }
 
 
-
-
 object OptionTable {
-
 
 
   val columns = List(
@@ -51,26 +54,61 @@ object OptionTable {
     Column[MutableOption]("Strike", true, _.strike.originalString,
       update = (option, newValue) => option.strike = newValue match {
         case s: String => ExpressionParser.parse(s)
-        case other => error(other.getClass + " is not supported for strike field")
-      }),
+        case other => sys.error(other.getClass + " is not supported for strike field")
+      },
+      customCellEditor = Some(new OptionTable.ExpressionCellEditor())),
     Column[MutableOption]("Quantity", true, _.quantity.originalString,
       update = (option, newValue) => option.quantity = newValue match {
         case s: String => ExpressionParser.parse(s)
-        case other => error(other.getClass + " is not supported for quantity field")
-      }),
+        case other => sys.error(other.getClass + " is not supported for quantity field")
+      },
+      customCellEditor = Some(new OptionTable.ExpressionCellEditor())),
     Column[MutableOption]("Notional", true, _.notional.originalString,
       update = (option, newValue) => option.notional = newValue match {
         case s: String => ExpressionParser.parse(s)
-        case other => error(other.getClass + " is not supported for notional field")
-      }),
+        case other => sys.error(other.getClass + " is not supported for notional field")
+      },
+      customCellEditor = Some(new OptionTable.ExpressionCellEditor())),
     Column[MutableOption]("BarrierType", true, _.optionBarrierType,
       update = (option, newValue) => option.optionBarrierType = newValue.asInstanceOf[OptionBarrierType],
       customCellEditor = Some(new OptionTable.ComboboxCellEditor(List(OptionBarrierType.NoBarrier, OptionBarrierType.KnockInBarrier, OptionBarrierType.KnockOutBarrier))))
   )
-  
 
 
   class ComboboxCellEditor(values: List[AnyRef]) extends DefaultCellEditor(new JComboBox(values.toArray))
+
+  class ExpressionCellEditor() extends DefaultCellEditor(new JTextField()) {
+
+    val originalBackgroundColor = editorComponent.getBackground
+
+    private def verify(textField: JTextField): Boolean = {
+      val textFieldType = ExpressionField
+      val isVerified = textFieldType.verify(textField.getText)
+
+      if (isVerified) {
+        textField.setBackground(originalBackgroundColor)
+        textField.setToolTipText("")
+      } else {
+        textField.setBackground(Color.RED)
+        textField.setToolTipText(textFieldType.notVerifiedMessage)
+      }
+
+      isVerified
+    }
+
+    override def stopCellEditing(): Boolean = {
+      //      println("stopCellEditing editorComponent: " + editorComponent)
+      //      val text = editorComponent.asInstanceOf[JTextField].getText
+      //      println("text: " + text)
+
+      val isVerified = verify(editorComponent.asInstanceOf[JTextField])
+      if (isVerified) {
+        super.stopCellEditing()
+      } else {
+        false
+      }
+    }
+  }
 
 }
 
